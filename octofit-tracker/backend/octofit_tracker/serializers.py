bc30461606ecaaa4f19a7ca22fbc88e7180af86e
ftakikaw <fkt_info@yahoo.com.br>
2026-03-19 21:45:59 +0000

    Initialize and create the octofit_db MongoDB database

---
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        if 'id' in rep:
+            rep['id'] = str(rep['id'])
+        if 'email' in rep:
+            rep['email'] = str(rep['email'])
+        return rep
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leaderboard
+        fields = '__all__'
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = '__all__'
---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='Marvel')
+        self.assertEqual(user.name, 'Test Hero')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='Avengers')
+        self.assertEqual(team.name, 'Avengers')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        activity = Activity.objects.create(user='Test Hero', activity_type='Running', duration=20)
+        self.assertEqual(activity.activity_type, 'Running')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        lb = Leaderboard.objects.create(team='Avengers', points=200)
+        self.assertEqual(lb.points, 200)
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Hero Training', description='Train like a hero.', difficulty='Hard')
+        self.assertEqual(workout.difficulty, 'Hard')
